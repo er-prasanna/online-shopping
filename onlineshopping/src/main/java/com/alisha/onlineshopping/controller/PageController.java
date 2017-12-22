b92960428c69c24c0699aeca7b6ffb9cabf4f06c
Prasanna Sinha <pksinha25@gmail.com>
2017-12-22 12:44:34 +0530

    Successfully loaded static resources

---
+++ onlineshopping/src/main/java/com/alisha/onlineshopping/controller/PageController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.alisha.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author psin82
+ *
+ */
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/index","/home"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Welcome to Spring Web MVC");
+		return mv;
+	}
+	
+}
